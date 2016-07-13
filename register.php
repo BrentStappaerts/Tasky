31d19d573ff19140f6a098b63e892313d8384500
IGenerate <brent.stappaerts@gmail.com>
2016-07-13 14:19:51 +0200

    add register.php

---
+++ register.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Tasky | Register</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="public/js/jquery-2.2.3.min.js"></script>
+    <link rel="stylesheet" href="public/css/bootstrap.min.css" type="text/css">
+    <script src="public/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="public/css/style.css" type="text/css">
+<body>
+    <div class="form--register">
+        <div class="col-sm-5 .col-md-6" id="logoForm">
+            <img src="public/images/TaskyLogo.png" width="100%" />
+        </div>
+        <div id="transparant">
+        <div class="col-sm-5 .col-md-6" id="registerForm">
+        <form action="" class="post__form--register" method="post">
+            <div class="form-group">
+                <input type="email" class="form-control" name="email" id="email" placeholder="Email">
+            </div>
+
+            <div class="form-group">
+                <input type="text" class="form-control"  name="Name" id="Name" placeholder="Name">
+            </div>
+
+            <div class="form-group">
+                <input type="password" class="form-control"  name="password" id="password" placeholder="Password">
+            </div>
+
+            <div class="form-group">
+                <input type="submit" class="btn btn-warning form--register__btn" value="Registreer nu!">
+            </div>
+        </form>
+    </div>
+    </div>
+
+    </div>
+    </div>
+	
+</body>
+</html>
