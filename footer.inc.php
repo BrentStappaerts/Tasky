1023098be3d6d215dd5de9d8e54c9ee9e9c789ab
IGenerate <brent.stappaerts@gmail.com>
2016-07-23 17:34:47 +0200

    Added footer en nav include

---
+++ footer.inc.php
@@ -0,0 +1,5 @@
+<footer>
+    <div>
+    <p>Made with &hearts; by <a href="http://www.igenerate.be">Brent Stappaerts</a> - &copy; 2016 TASKY</p>
+    </div>
+</footer>
---
+++ nav.inc.php
@@ -0,0 +1,5 @@
+    <nav>
+    <img src="public/images/TaskyLogo.png" width="10%" />
+    <a href="logout.php">Uitloggen</a>
+    <a href="add.php">Toevoegen</a>
+    </nav>
