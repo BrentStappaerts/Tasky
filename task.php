5746c6547c8b5b63d0e1d77d345ee4c170e68c51
IGenerate <brent.stappaerts@gmail.com>
2016-07-28 12:40:27 +0200

    add task.php

---
+++ task.php
@@ -0,0 +1,53 @@
+<?php
+    session_start();
+    spl_autoload_register(function ($class_name) {
+        include 'classes/' .$class_name . '.class.php';
+    });
+
+    if(isset($_SESSION['loggedin']) && isset($_SESSION['user_id'])){
+        $user = new User();
+        $user_id = $_SESSION['user_id'];
+
+        $db =  Db::getInstance();
+        $stmt = $db->prepare("SELECT * FROM users WHERE user_id=:user_id");
+        $stmt->execute(array(":user_id"=>$user_id));  
+        $userRow=$stmt->fetch(PDO::FETCH_ASSOC);
+    }
+    else {
+        header('Location: index.php');
+    }
+    $deadline = new Deadline();
+
+?><!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Tasky | Lijst</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="public/js/jquery-2.2.3.min.js"></script>
+    <link rel="stylesheet" href="public/css/bootstrap.min.css" type="text/css">
+    <script src="public/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="public/css/style.css" type="text/css">
+</head>
+<body>
+    <?php include 'nav.inc.php'; ?></div>
+    <div id="timeline">
+        <div class="col-sm-5 .col-md-6" id="leftbalk">
+            <img src="public/images/profile.png"/>
+            <div class="col-sm-5 .col-md-6" id="gegevens">
+                <h5><?php print($userRow['name']); ?></h5>
+            </div>
+            <div class="col-sm-5 .col-md-6" id="settings">
+                <a href=""><img src="public/images/settings.png" /></a>
+            </div>  
+
+        </div>
+        <div class="col-sm-5 .col-md-6" id="home">
+                <h5>Test</h5>
+        </div>
+
+    </div>
+</div>
+
+</body>
+</html>
