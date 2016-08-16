cfbb0fab0f0d793c3200e3b7c6c45dc0385a7578
IGenerate <brent.stappaerts@gmail.com>
2016-08-16 17:10:06 +0200

    Edited nav.inc.php, navigatie aangepast voor admin

--- nav.inc.php
+++ nav.inc.php
@@ -1,5 +1,12 @@
-    <nav>
-    <img src="public/images/TaskyLogo.png" width="10%" />
-    <a href="logout.php">Uitloggen</a>
-    <a href="add.php">Lijst toevoegen</a>
-    </nav>
+<nav>
+<img src="public/images/TaskyLogo.png" width="10%" />
+<a href="logout.php">Uitloggen</a>
+<a href="add.php">Lijst toevoegen</a>
+<?php
+if ($userRow['admin'] == 1) {
+	echo "<a href='admin.php'>Vakken toevoegen</a>";
+} else {
+	//do nothing
+}
+?>
+</nav>
