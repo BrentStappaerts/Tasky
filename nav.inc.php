6eb34fc10fefbc45e3f1f33529fe3cbae180b1b1
IGenerate <brent.stappaerts@gmail.com>
2016-08-17 17:01:33 +0200

    edited nav.inc, logo linken aan homepagina

--- nav.inc.php
+++ nav.inc.php
@@ -1,5 +1,5 @@
 <nav>
-<img src="public/images/TaskyLogo.png" width="10%" />
+<a href="home.php" id="logo" ><img src="public/images/TaskyLogo.png" width="25%" /></a>
 <a href="logout.php">Uitloggen</a>
 <a href="add.php">Lijst toevoegen</a>
 <?php
